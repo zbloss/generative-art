810ba87efe48194880a92b7ee3c613a63a4ca6cb
Zach Bloss <zacharybloss@gmail.com>
2021-08-25 09:01:13 -0400

    add tqdm as dependency
    add script to convert rgba to rgb files

--- src/data/convert_rgba_to_rgb.py
+++ src/data/convert_rgba_to_rgb.py
@@ -2,6 +2,7 @@ from PIL import Image
 import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from tqdm import tqdm
 
 def logger(name, ten):
     print(f'{name}: {ten}')
@@ -9,19 +10,12 @@ def logger(name, ten):
 @hydra.main(config_path="../configs", config_name="monsters_config")
 def convert_rgba_to_rgb(cfg : DictConfig) -> None:
 
+    png_image_files = glob(os.path.join(cfg.data.path_to_image_files, '**/*.png'), recursive=True)
+    for png_image in tqdm(png_image_files, desc="Converting RGBA to RGB Images", total=len(png_image_files)):
+        image = Image.open(png_image)
+        del image.info['transparency']
+        image.save(png_image)
 
 
-src = "./resizedData"
-dst = "./resized_black/"
-
-for each in os.listdir(src):
-    png = Image.open(os.path.join(src,each))
-    # print each
-    if png.mode == 'RGBA':
-        png.load() # required for png.split()
-        background = Image.new("RGB", png.size, (0,0,0))
-        background.paste(png, mask=png.split()[3]) # 3 is the alpha channel
-        background.save(os.path.join(dst,each.split('.')[0] + '.jpg'), 'JPEG')
-    else:
-        png.convert('RGB')
-        png.save(os.path.join(dst,each.split('.')[0] + '.jpg'), 'JPEG')
+if __name__ == "__main__":
+    convert_rgba_to_rgb()
